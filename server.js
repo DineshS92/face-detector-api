7ea66322fb5d8ac48ca05947f8c62b4b3c956f28
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-04 23:07:08 +0530

    connecting the database and endpoints

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 const knex = require('knex')({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
     user: 'postgres',
-    password: '',
+    password: 'test@1234',
     database: 'smartbrain'
   } 
 });
 
-// console.log(knex.select('*').from('users'));
+// knex.select('*').from('users').then(data => {
+//   console.log(data);
+// });
 
 const app = express();
 
@@ -53,43 +56,51 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  datab.users.push({
-    id: '11',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(datab.users[datab.users.length - 1]);
+  // datab.users.push({
+  //   id: '11',
+  //   name: name,
+  //   email: email,
+  //   entries: 0,
+  //   joined: new Date()
+  // });
+  knex('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+      .then(user => {
+        res.json(user[0]);
+    })
+      .catch(err => res.status(400).json('Your Request Could not be Fulfilled'));
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  datab.users.forEach(user => {
-    if(user.id === id) {
-      found = true;
-      return res.json(user);
+  knex('users').where({
+    id: id
+  }).select('*')
+  .then(user => {
+    if(user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('User Not Found');
     }
   });
-  if(!found) {
-    res.status(404).json('User Does not Exist');
-  }
 });
+
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  datab.users.forEach(user => {
-    if(user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if(!found) {
-    res.status(404).json('User Does not Exist');
-  }
+  knex('users')
+  .where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(Number(entries[0]));
+  })
+  .catch(err => res.status(400).json('Bad Request'));
 });
 
 app.listen(3000, () => {
